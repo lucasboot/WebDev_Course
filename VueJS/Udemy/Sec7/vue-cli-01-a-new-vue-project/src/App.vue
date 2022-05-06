226b8e4edad6307c6689d6b0f640b07790159606
lucasboot <varela.alves@outlook.com>
2022-05-05 21:47:10 -0300

    sec8 to go

---
+++ VueJS/Udemy/Sec7/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <section>
+        <h2>My friends</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            friends: [
+                {
+                    id: 'igor',
+                    name: 'Igor Renoir',
+                    phone: '0120310',
+                    email: 'igor@bff.com.br'
+                },
+                {
+                    id: 'betty',
+                    name: 'Betty Liz',
+                    phone: '0120310',
+                    email: 'betty@bff.com.br'
+                },
+                {
+                    id: 'daniel',
+                    name: 'Daniel Souza Lins',
+                    phone: '01203121210',
+                    email: 'daniel@bff.com.br'
+                }
+
+
+            ]
+        }
+    }
+}
+
+</script>
